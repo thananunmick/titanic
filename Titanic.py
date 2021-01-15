be8bb94b1352b2ec613167873f035f60422b60ab
Mick Kanchanachayphoom <thananun.mick@gmail.com>
2021-01-16 00:45:29 +0700

    Include evaluation method using precision_recall_curve and roc_auc_score

--- Titanic.py
+++ Titanic.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import os
+import matplotlib.pyplot as plt
 from sklearn.preprocessing import OrdinalEncoder
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import StandardScaler
@@ -9,6 +10,8 @@ from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import cross_val_predict
 from sklearn.model_selection import GridSearchCV
 from sklearn.svm import SVC
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import precision_score, recall_score, precision_recall_curve, roc_auc_score
 
 # load training dataset and return it as a pandas dataframe
 def load_train_set():
@@ -55,53 +58,84 @@ def run_kneighbor_clf():
     grid = GridSearchCV(knn_clf, grid_param)
     return grid.fit(X_train_final, y_train)
 
-# grid = run_kneighbor_clf()
-
 def run_svc_clf():
-    grid_param = {'kernel': ['poly', 'rbf', 'sigmoid'], 'degree': [1, 2, 3, 4, 5, 6, 7, 8]}
+    grid_param = {'kernel': ['poly', 'rbf', 'sigmoid', 'linear'], 'degree': [1, 2, 3, 4, 5, 6, 7, 8, 9]}
     svc_clf = SVC()
     grid = GridSearchCV(svc_clf, grid_param, cv=3, scoring="accuracy")
     return grid.fit(X_train_final, y_train)
 
-grid = run_svc_clf()
+def run_log_reg_clf():
+    log_reg = LogisticRegression(penalty='l2', C=50)
+    return log_reg.fit(X_train_final, y_train)
+
+# Main train function
+def train():
+    # model = run_kneighbor_clf()
+    model = run_svc_clf()
+    # model = run_log_reg_clf()
+    return model
+
+model = train()
 
 print("Done Training")
-print(grid.best_params_)
+print(model.best_estimator_)
 
-# Use CrossValScore to test the accruacy of the model
-print(cross_val_score(grid, X_train_final, y_train, cv=3, scoring="accuracy"))
+# print(y_train.value_counts())
+
+# Evaluation
+def evaluate():
+    # Use CrossValScore to test the accruacy of the model
+    print(cross_val_score(model, X_train_final, y_train, cv=3, scoring="accuracy"))
+
+    y_pred = cross_val_predict(model, X_train_final, y_train, cv=3)
+    # Printing Precision and Recall score
+    print("Precision score: ", precision_score(y_train, y_pred))
+    print("Recall score: ", recall_score(y_train, y_pred))
+    precision, recall, thresholds = precision_recall_curve(y_train, y_pred)
+    plt.plot(recall, precision, "b-")
+    plt.ylabel("Precision")
+    plt.xlabel("Recall")
+
+    # Compute and print ROC AUC score
+    roc_auc = roc_auc_score(y_train, y_pred)
+    print("ROC_AUC score: ", roc_auc)
+
+evaluate()
 
 X_train_predict = X_train_final[19]
-print(grid.predict([X_train_predict]))
+print(model.predict([X_train_predict]))
+
+plt.show()
 
 ###############################################################
 #Test set predictions
-def load_test_set():
-    test_path = os.path.join("dataset", "test.csv")
-    return pd.read_csv(test_path)
+def run_test_set():
+    def load_test_set():
+        test_path = os.path.join("dataset", "test.csv")
+        return pd.read_csv(test_path)
 
-test = load_test_set()
-test_pass_id = test[["PassengerId"]].copy().to_numpy()
-test_nm = test.drop(["Name", "Ticket", "Cabin", "PassengerId"], axis=1)
-test_nm = test_nm.fillna(test_nm["Age"].mean())
-test_cat = test[['Sex', 'Embarked']].copy()
-test_nm = test_nm.drop(['Sex', 'Embarked'], axis=1)
-test_cat_encoded = oe.fit_transform(test_cat)
-test_cat_encoded_df = pd.DataFrame(test_cat_encoded, columns=test_cat.columns, index=test_cat.index)
-# print(len(test_nm), len(test_cat_encoded_df))
-# print(test_nm.head(), test_cat_encoded_df.head())
-test_final = test_nm.join(test_cat_encoded_df)
-test_final = scaler.fit_transform(test_final)
+    test = load_test_set()
+    test_pass_id = test[["PassengerId"]].copy().to_numpy()
+    test_nm = test.drop(["Name", "Ticket", "Cabin", "PassengerId"], axis=1)
+    test_nm = test_nm.fillna(test_nm["Age"].mean())
+    test_cat = test[['Sex', 'Embarked']].copy()
+    test_nm = test_nm.drop(['Sex', 'Embarked'], axis=1)
+    test_cat_encoded = oe.fit_transform(test_cat)
+    test_cat_encoded_df = pd.DataFrame(test_cat_encoded, columns=test_cat.columns, index=test_cat.index)
+    # print(len(test_nm), len(test_cat_encoded_df))
+    # print(test_nm.head(), test_cat_encoded_df.head())
+    test_final = test_nm.join(test_cat_encoded_df)
+    test_final = scaler.fit_transform(test_final)
 
-print(test_nm['Age'].isnull().values.any())
+    print(test_nm['Age'].isnull().values.any())
 
-# prediction = cross_val_predict(grid, test_final, cv=3)
-# print(prediction)
-test_final_first = test_final[0]
-prediction = grid.predict(test_final)
-prediction = prediction.reshape(418, 1)
-prediction = np.c_[test_pass_id, prediction]
-prediction = prediction.astype(np.int32)
-# np.savetxt("Prediction.csv", prediction, delimiter=",", fmt="%i")
-# print(prediction)
-# print(test_final)
+    # prediction = cross_val_predict(grid, test_final, cv=3)
+    # print(prediction)
+    test_final_first = test_final[0]
+    prediction = model.predict(test_final)
+    prediction = prediction.reshape(418, 1)
+    prediction = np.c_[test_pass_id, prediction]
+    prediction = prediction.astype(np.int32)
+    # np.savetxt("Prediction.csv", prediction, delimiter=",", fmt="%i")
+    # print(prediction)
+    # print(test_final)
